0c00f0160a462b4d075795b70156ba0dec7ed611
umairahmedbajwa <umairahmed.bajwa@systemsltd.com>
2021-11-19 19:12:40 +0500

    made responsive

--- js/script.js
+++ js/script.js
@@ -12,23 +12,49 @@
 
 window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("scrollFix");
-var sticky = navbar.offsetTop;
+let navbar = document.getElementById("scrollFix");
+let sticky1 = navbar.offsetTop;
 let topnavRight = document.getElementById("slide-right");
 let topnavLeft = document.getElementById("slide-left");
 
+// let slideDown = document.getElementById("scrollNav");
+// let slideDdown = document.getElementsByTagName("nav");
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > sticky1) {
     navbar.classList.add("sticky");
     topnavRight.classList.remove("animation-right");
     topnavLeft.classList.remove("animation-left");
   } else {
-    navbar.classList.remove("sticky");
-    topnavRight.classList.add("animation-right");
+    
+    // slideDdown.classList.add("nav-animation");
     topnavLeft.classList.add("animation-left");
+    topnavRight.classList.add("animation-right");
+    setTimeout(function () {
+      // $("#header").removeClass("hidden");
+      navbar.classList.remove("sticky");
+    }, 1700);
+
+
   }
 }
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+//     document.getElementById("scrollNav").style.top = "0px";
+    
+//     // setTimeout(function () {
+//       // document.getElementById("scrollNav").style.top = "-140px";
+//       // }, 2000);
+//     } 
+//     else {
+//     document.getElementById("scrollNav").style.top = "210px";
+//   }
+// }
+
 
 function listCart() {
   var x = document.getElementById("cartItems");
